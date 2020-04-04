4bbc70a231c9a04415186ecae9dc9a88876a6985
sksaifuddin <atushaik@gmail.com>
2020-04-04 23:02:04 +0530

    completed HTML and CSS changes, Completed basic structure of the view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SideBarSectionComponent } from './components/side-bar-section/side-bar-section.component';
 import { NotesMainSectionComponent } from './components/notes-main-section/notes-main-section.component';
