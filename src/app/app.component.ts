a0539ac00fda39c3e3a808fadcdc8aa84d944ef8
sksaifuddin <atushaik@gmail.com>
2020-04-07 01:02:44 +0530

    changed design, added collapsible button and added logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notesTaking';
+  displaySideBar: boolean = true;
+
+  onExpandBtnClick(): void {
+    this.displaySideBar = !this.displaySideBar;
+  }
+
 }
