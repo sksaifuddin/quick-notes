d4aec381fbed4965cae0929ef0bc3fb6c9d9fb10
sksaifuddin <atushaik@gmail.com>
2020-04-12 19:29:36 +0530

    Completed highlighting the text on search functionality

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -9,10 +9,27 @@ export class SearchPipe implements PipeTransform {
     if (!args) {
       return value;
     }
-    const notesWithSearch: Notes[] = value.filter(
-      (notes: Notes) =>
-        notes.notesText.toLowerCase().search(args.toLowerCase()) !== -1
-    );
-    return notesWithSearch;
+    const notesWithSearch: Notes[] = this.getNotesWithSearchedTxt(value, args);
+    const highlightedNotes: Notes[] = this.getNotesWithHighlightedTxt(notesWithSearch, args);
+    return highlightedNotes;
   }
+
+  getNotesWithSearchedTxt(notes: Notes[], searchText: string): Notes[] {
+    return notes.filter(
+      (notes: Notes) =>
+        notes.notesText.toLowerCase().search(searchText.toLowerCase()) !== -1
+    );
+  }
+
+  getNotesWithHighlightedTxt(notesWithSearch: Notes[], searchText: string): Notes[] {
+    return notesWithSearch.map((notes: Notes) => {
+      // gi used to do a case insensitive search of all occurrences of a regular expression in a string.
+      const re = new RegExp(searchText, 'gi');
+      return {
+        ...notes,
+        notesText: notes.notesText.replace(re, `<span class="text-warning">${searchText}</span>` )
+      }
+    })
+  }
+
 }
